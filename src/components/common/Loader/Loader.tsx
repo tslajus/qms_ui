0af79cf0b18bbef43eef535fdbf0ce082cdb7d2f
Tadas Šlajus <tslajus@gmail.com~>
2023-08-21 13:24:10 +0300

    Loader fixed;

--- src/components/common/Loader/Loader.tsx
+++ src/components/common/Loader/Loader.tsx
@@ -1,16 +1,8 @@
 import styles from "./Loader.module.scss";
 
-interface LoaderProps {
-  isFullScreen?: boolean;
-}
-
-function Loader({ isFullScreen = true }: LoaderProps) {
-  const loaderClassName = !isFullScreen
-    ? styles["full-screen"]
-    : styles["loader--full-screen"];
-
+function Loader() {
   return (
-    <div className={loaderClassName}>
+    <div className={styles.loader}>
       <div className={styles["loader__ring"]}>
         <div></div>
         <div></div>
